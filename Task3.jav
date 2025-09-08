63c5870e4a8c5dbff19aa90f1d8ca66cfd0591a1
ABEEGOLD <abeeutee@gmail.com>
2025-09-08 03:17:41 +0100

    tasks

--- Task3.jav
+++ Task3.jav
@@ -1,28 +1,23 @@
+import java.util.Scanner;
 public class Task3{
 	public static void main(String[]args){
-
+		Scanner input = new Scanner(System.in);
 		int sum = 0;
 		int averageScore = 0;
 
 		for(int index = 0;index < 10;index++){
 			index++;
 			
-			System.out.print("Enter number : ");
+			System.out.print("Enter  ten number : ");
 			int score = input.nextInt();
 			
-			sum += number;
+			sum += score;
 			averageScore = sum / 10;
 
-
-
-
+			}
 			System.out.println("The number sum is:" +sum);
 			Systm.out.println("The number average is:"+averageScore);
-			
-			
-			
 
-		}
 
 	}
 
--- src/ComparingInteger.java
+++ src/ComparingInteger.java
@@ -1,37 +1,37 @@
-import java.util.Scanner;
-
-public class ComparingInteger {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = input.nextInt();
-
-        int square = number * number;
-
-        if (number > 100 && square >100) {
-            System.out.printf("The %d  is greater than 100:", number);
-        }
-
-        if (number == 100 && square == 100) {
-            System.out.printf("The %d  is equal than 100:", number);
-        }
-
-        if (number != 100 && square != 100) {
-            System.out.printf("The %d  is less than 100:", number);
-        }
-
-        if (number < 100 && square < 100) {
-            System.out.printf("The %d is less than 100:", number);
-        }
-        //if (number != 100)
-            //System.out.printf("is equal to 100: %d == %d%n", number,100);
-        //}else{
-            //System.out.printf("is not equal to 100",)
-
-
-
-
-
-
-    }
-}
+//import java.util.Scanner;
+//
+//public class ComparingInteger {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int number = input.nextInt();
+//
+//        int square = number * number;
+//
+//        if (number > 100 && square >100) {
+//            System.out.printf("The %d  is greater than 100:", number);
+//        }
+//
+//        if (number == 100 && square == 100) {
+//            System.out.printf("The %d  is equal than 100:", number);
+//        }
+//
+//        if (number != 100 && square != 100) {
+//            System.out.printf("The %d  is less than 100:", number);
+//        }
+//
+//        if (number < 100 && square < 100) {
+//            System.out.printf("The %d is less than 100:", number);
+//        }
+//        //if (number != 100)
+//            //System.out.printf("is equal to 100: %d == %d%n", number,100);
+//        //}else{
+//            //System.out.printf("is not equal to 100",)
+//
+//
+//
+//
+//
+//
+//    }
+//}
